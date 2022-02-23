d85de9cbd154ed1d2fb0546190e141bebfd442b7
Joshua C. Willis <3475947+JasherIO@users.noreply.github.com>
2022-02-22 18:46:53 -0600

    Post SEO

--- app/routes/blog/$slug.tsx
+++ app/routes/blog/$slug.tsx
@@ -9,12 +9,15 @@ import { platforms } from "~/utilities/site";
 export const meta: MetaFunction = ({ data }) => {
   const { frontmatter } = data;
   const twitter = platforms.find(platform => platform.name === "Twitter");
+  const date = new Date(frontmatter.date);
 
   return { 
     title: `Jasher | ${frontmatter.title}`,
     description: frontmatter.description,
     keywords: frontmatter.keywords.join(", "),
-    "twitter:card": "summary",
+    thumbnail: frontmatter.image,
+    pubdate: date.toISOString(),
+    "twitter:card": "summary_large_image",
     "twitter:creator": twitter?.id || "",
     "twitter:description": frontmatter.description,
     "twitter:image": frontmatter.image,
@@ -22,7 +25,9 @@ export const meta: MetaFunction = ({ data }) => {
     "twitter:title": frontmatter.title,
     "og:description": frontmatter.description,
     "og:image": frontmatter.image,
-    "og:site_name": frontmatter.title,
+    "og:image:width": "1920",
+    "og:image:height": "1080",
+    "og:pubdate": date.toISOString(),
     "og:title": frontmatter.title,
     "og:type": "article",
     // "og:url": "",
@@ -36,10 +41,12 @@ export const loader: LoaderFunction = async ({ params }) => {
 
 export default function Post() {
   const { frontmatter, html } = useLoaderData<Post>();
+  const date = new Date(frontmatter.date);
 
   return (
     <Section>
       <article className="prose md:prose-lg dark:prose-invert prose-neutral mx-auto">
+        <time dateTime={date.toISOString()}></time>
         <h1>{frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: html }}></div>
       </article>
