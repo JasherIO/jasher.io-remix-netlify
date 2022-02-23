dc69697eb35f15ef9afd63d1a7a028bc4d3da502
Joshua C. Willis <3475947+JasherIO@users.noreply.github.com>
2022-02-23 17:56:41 -0600

    removed undeeded meta declarations

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,14 +1,5 @@
-import type { MetaFunction } from "remix";
 import Hero from "~/components/Hero";
 import FeaturedProjects from "~/components/FeaturedProjects";
-
-// https://remix.run/api/conventions#meta
-export let meta: MetaFunction = () => {
-  return {
-    title: "Jasher",
-    description: "Hello, I'm Josh!"
-  };
-};
 
 export default function Index() {
   return (
